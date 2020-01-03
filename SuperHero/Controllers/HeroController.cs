368a84f195c47830b6995b17689124315864d1e0
Michael Gonzalez Jr <mikegjr95@gmail.com>
2020-01-03 11:46:49 -0600

    init

---
+++ SuperHero/Controllers/HeroController.cs
@@ -0,0 +1,109 @@
+﻿using SuperHero.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperHero.Controllers
+{
+    public class HeroController : Controller
+    {
+        ApplicationDbContext db;
+        public HeroController()
+        {
+            db = new ApplicationDbContext();
+        }
+        // GET: Hero
+        public ActionResult Index()
+        {
+            var allHeroes = db.Heroes;
+            return View(allHeroes);
+        }
+
+        // GET: Hero/Details/5
+        public ActionResult Details(int id)
+        {
+            Hero hero = db.Heroes.Where(h => h.Id == id).Select(h => h).FirstOrDefault();
+            return View(hero);
+        }
+
+        // GET: Hero/Create
+        public ActionResult Create()
+        {
+            Hero hero = new Hero();
+            return View(hero);
+        }
+
+        // POST: Hero/Create
+        [HttpPost]
+        public ActionResult Create(Hero hero)
+        {
+            try
+            {
+                db.Heroes.Add(hero);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Hero/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Hero hero = db.Heroes.Where(h => h.Id == id).Select(h => h).FirstOrDefault();
+            return View(hero);
+        }
+
+        // POST: Hero/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Hero hero)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                var newHero = db.Heroes.Where(h => h.Id == hero.Id).Select(h => h).Single();
+                newHero.firstName = hero.firstName;
+                newHero.lastName = hero.lastName;
+                newHero.primaryAbility = hero.primaryAbility;
+                newHero.secondaryAbility = hero.secondaryAbility;
+                newHero.alterEgo = hero.alterEgo;
+                newHero.catchphrase = hero.catchphrase;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Hero/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Hero hero = db.Heroes.Where(h => h.Id == id).Select(h => h).FirstOrDefault();
+            return View(hero);
+        }
+
+        // POST: Hero/Delete/5
+        [HttpPost]
+        public ActionResult Delete(Hero hero)
+        {
+            try
+            {
+                Hero NewHero = db.Heroes.Where(h => h.Id == hero.Id).Select(h => h).FirstOrDefault();
+                db.Heroes.Remove(NewHero);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SuperHero/Models/Hero.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SuperHero.Models
+{
+    public class Hero
+    {
+        [Key]
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string alterEgo { get; set; }
+        public string primaryAbility { get; set; }
+        public string secondaryAbility { get; set; }
+        public string catchphrase { get; set; }
+
+    }
+}
